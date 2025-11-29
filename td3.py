673c07168cb55873477284ec0888ba9f8027a8f7
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2025-11-29 15:36:34 +0900

    finished td3 with 100 lines

--- td3.py
+++ td3.py
@@ -1,254 +1,100 @@
-import os
-import random
-import time
-from dataclasses import dataclass
+import gymnasium as gym, torch as T, torch.nn as nn, torch.nn.functional as F
 
-import gymnasium as gym
-import nympy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import tyro
-from torch.utils.tensorboard import SummaryWriter
+seed, learning_starts, total_timesteps, buffer_limit, batch_size, learning_rate, gamma, policy_noise, policy_frequency, noise_clip, action_scale, exploration_noise, tau = 0, 25e3, int(1e6), int(1e6), 256, 3e-4, 0.005, 0.1, 2, 0.5, T.tensor(2, dtype=T.float32), 0.1, 0.005
 
-from cleanrl_utils.buffers import ReplayBuffer
+class ReplayBuffer:
+    def __init__(self):
+        self.s, self.a, self.r, self.sp, self.d = [T.empty(0, i) for i in [3, 1, 1, 3, 1]]  # state, action, reward, s prime, done
 
+    def push(self, transition):
+        s, a, r, sp, d = transition
+        self.s, self.a, self.r, self.sp, self.d = [
+            T.cat((T.Tensor(new).unsqueeze(0), buffer), 0)[:buffer_limit]
+            for new, buffer in zip([s, a, [r], sp, [d]], [self.s, self.a, self.r, self.sp, self.d])
+        ]
 
-@dataclass
-class Args:
+    def sample(self, n):
+        idx = T.randperm(len(self))[:n]
+        return {'s': self.s[idx], 'a': self.a[idx], 'r': self.r[idx], 'sp': self.sp[idx], 'done': self.d[idx]}
 
-    # __file__ is a variable which contains a running script's path(absolute or relative)
-    exp_name: str = os.path.basename(__file__)[: -len(".py")]
-    seed: int = 1
-    torch_deterministic: bool = True
-    cuda: bool = True
-    track: bool = False
-    wandb_project_name: str = "cleanRL"
-    wandb_entity: str = None
-    capture_video: bool = False
-    upload_model: bool = False
-    hf_entity: str = ""
+    def __len__(self): return self.s.shape[0]
 
-    # Algorithm specific arguments
-    env_id: str = "Hopper-v4"
-    total_timesteps: int = 1000000
-    learning_rate: float = 3e-4
-    num_envs: int = 1
-    buffer_size: int = int(1e6)
-    gamma: float = 0.005
-    batch_size: int = 256
-    policy_noise: float = 0.1
-    learning_starts int = 25e3
-    policy_frequency: int = 2
-    noise_clip: float = 0.5
-
-
-def make_env(env_id, seed, idx, capture_video, run_name):
-    def tuunk():
-        if capture_video and idx == 0:
-            env = gym.make(env_id, render_mode="rgb_array")
-            env = gym.wrappers.RecordVideo(env, f"video/{run_name}")
-        else:
-            env = gym.make(env_id)
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env.action_space.seed(seed)
-        return env
-
-    return thunk
-
-class QNetwork(nn.Module):
-    def __init__(self, env):
+class QNet(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(
-            # prod means multiplying every value of shape (e.g. (81, 81, 3).prod == 81*81*3.
-            np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape),
-            256,
-        )
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
+        self.fc = nn.Sequential(nn.Linear(4, 256), nn.ReLU(), nn.Linear(256, 1))
 
-    def forward(self, x, a):
-        x = torch.cat([x, a], 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    def forward(self, x, a): return self.fc(T.cat([x, a], 1))
+
+class PolicyNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc_mu = nn.Sequential(nn.Linear(3, 256), nn.ReLU(), nn.Linear(256, 1))
+
+    def forward(self, x): return T.tanh(self.fc_mu(x))
 
 if __name__ == "__main__":
 
-    args = tyro.cli(Args)
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.track:
-        import wandb
-
-        wandb.init(
-            project=args.wandb_project_name,
-            entity=args.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(args),
-            name=run_name,
-            monitor_gym=True,
-            save_code=True,
-        )
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()]))
-
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    '''Whether PyTorch's cuDNN backend uses deterministic
-    Same input -> Same output  
-    Results are reproducible across different runs
-    '''
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
-    device = torch.device("cuda" if torch.cuda_is_available() and args.cuda else "cpu")
-
-    envs = gym.vector.SyncVectorEnv(
-        [make_env(args.env_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)]
-    )
-    assert isinstance(envs.single_action_spacde, gym.spaces.Box), "only continuous action space is supported"
-
-    actor = Actor(envs).to(device)
-    qf1 = QNetwork(envs).to(device)
-    qf2 = QNetwork(envs).to(device)
-    qf1_target = QNetwork(envs).to(device)
-    qf2_target = QNetwork(envs).to(device)
-    target_actor = Actor(envs).to(device)
-    target_actor.load_state_dict(actor.state_dict())
+    env = gym.make('Pendulum-v1')
+    actor = PolicyNet()
+    qf1, qf1_target, qf2, qf2_target = [QNet() for _ in range(4)]
     qf1_target.load_state_dict(qf1.state_dict())
     qf2_target.load_state_dict(qf2.state_dict())
-    q_optimizer = optim.Adam(list(qf1.parameters()) + list(qf2.parameters()), lr=args.learning_rate)
-    actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
+    actor_target = PolicyNet()
+    actor_target.load_state_dict(actor.state_dict())
+    q_optimizer = T.optim.Adam(list(qf1.parameters()) + list(qf2.parameters()), lr=learning_rate)
+    actor_optimizer = T.optim.Adam(list(actor.parameters()), lr=learning_rate)
 
-    envs.single_observation_space.dtype = np.float32
-    rb = ReplayBuffer(
-        args.buffer_size,
-        envs.single_observation_space,
-        envs.single_action_space,
-        device,
-        n_envs=args.num_envs,
-        handle_timeout_termination=False,
-    )
-    start_time = time.time()
+    rb = ReplayBuffer()
 
-    obs, _ = envs.reset(seed=args.seed)
-    for global_step in range(args.total_timesteps):
-        if global_step < args.learning_starts:
-            # Random actions before args.learning_starts
-            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
+    obs, _ = env.reset(seed=seed)
+    score = 0.
+    for global_step in range(total_timesteps):
+        if global_step < learning_starts:
+            action = env.action_space.sample()
         else:
-            with torch.no_grad():
-                actions = actor(torch.Tensor(obs).to(device))
-                actions += torch.normal(0, actor.action_scale * args.exploration_noise)  # Smoothing q funciton
-                actions = actions.cpu().numpy().clip(envs.single_action_space.low, envs.single_action_space.high)
+            with T.no_grad():
+                action = actor(T.Tensor(obs))
+                action += T.normal(0, action_scale * exploration_noise)  # Smoothing q funciton
+                action = action.cpu().numpy().clip(env.action_space.low, env.action_space.high)
         
-        next_obs, rewards, terminations, truncations, infos = envs.step(actions)
-
-        if "final_info" in infos:
-            for info in infos['final_info']:
-                if info is not None:
-                    print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
-                    writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
-                    writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
-                    break
-
+        next_obs, reward, done, _, _ = env.step(action)
         real_next_obs = next_obs.copy()
-        for idx, trunc in enumerate(turncations):
-            if trunc:
-                real_next_obs[idx] = infos["final_observation"][idx]
-        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
-
+        score += reward
+        rb.push((real_next_obs, action, reward, obs, done))
         obs = next_obs
 
-        if global_step > args.learning_starts:
-            data = rb.sample(args.batch_size)
-            with torch.no_grad():
-                clipped_noise = (torch.randn_like(data.actions, device=device) * args.policy_noise).clamp(
-                    -args.noise_clip, args.noise_clip
-                ) * target_actor.action_scale
-
-                next_state_actions = (target_actor(data.next_observations) + clipped_noise).clamp(
-                    envs.single_aciton_space.low[0], envs.single_action_space.high[0]
+        if global_step > learning_starts:
+            data = rb.sample(batch_size)
+            with T.no_grad():
+                clipped_noise = (T.randn_like(data['a']) * policy_noise).clamp(
+                    -noise_clip, noise_clip
+                ) * action_scale
+                next_state_action = (actor_target(data['s']) + clipped_noise).clamp(
+                    env.action_space.low[0], env.action_space.high[0]
                 )
-                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
-                qf2_next_target = qf2_target(data.next_observations, next_state_actions)
-                min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
-                next_q_value = data.rewards.flatten() + (1 - data.dones.flatten()) * args.gamma * (min_qf_next_target).view(-1)
-
-            qf1_a_values = qf1(data.observations, data.actions).view(-1)
-            qf2_a_values = qf2(data.observations, data.actions).view(-1)
-            qf1_loss = F.mse_loss(qf1_a_values, next_q_values)
-            qf2_loss = F.mse_loss(qf2_a_values, next_q_values)
-            qf_loss = qf1_loss + qf2_loss
+                qf1_next_target, qf2_next_target = [q_target(data['s'], next_state_action) for q_target in [qf1_target, qf2_target]]
+                min_qf_next_target = T.min(qf1_next_target, qf2_next_target)
+                next_q_value = (data['r'] + gamma * (1 - data['done']) * min_qf_next_target).view(-1)
+            qf1_a_value, qf2_a_value = [qf(data['sp'], data['a']).view(-1) for qf in [qf1, qf2]]
+            qf_loss = F.mse_loss(qf1_a_value, next_q_value) + F.mse_loss(qf2_a_value, next_q_value)
 
             q_optimizer.zero_grad()
             qf_loss.backward()
             q_optimizer.step()
 
-            if global_step % args.policy_frequency == 0:  # Delayed Policy Update
-                actor_loss = -qf1(data.observations, actor(data.observations)).mean()
+            if global_step % policy_frequency == 0:  # Delayed Policy Update
+                actor_loss = -qf1(data['sp'], actor(data['sp'])).mean()
                 actor_optimizer.zero_grad()
                 actor_loss.backward()
                 actor_optimizer.step()
 
                 # Polyak averaging
-                for param, target_param in zip(actor.parameters(), target_actor.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-                for param, target_param in zip(actor.parameters(), target_actor.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
-                for param, target_param in zip(actor.parameters(), target_actor.parameters()):
-                    target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)
+                for nn, nn_target in zip([actor, qf1, qf2], [actor_target, qf1_target, qf2_target]):
+                    for param, param_target in zip(nn.parameters(), nn_target.parameters()):
+                        param_target.data.copy_(tau * param.data + (1 - tau) * param_target.data)
 
-            if global_step % 100 == 0:
-                writer.add_scalar("losses/qf1_values", qf1_a_values.mean().item(), global_step)
-                writer.add_scalar("losses/qf2_values", qf2_a_values.mean().item(), global_step)
-                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
-                writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss,item(), global_step)
-                print("SPS:", int(global_step / (time.time() - start_time)))
-                writer.add_scalar(
-                    "charts/SPS",
-                    int(global_step / (time.time() - start_time)),
-                    global_step,
-                )
-
-        if args.save_model:
-            model_path = f"runs/{run_name}/{args.exp_name}.cleanrl_model"
-            torch.save((actor.state_dict(), qf1.state_dict(), qf2.state_dict()), model_path)
-            print(f"model saved to {model_path}")
-            from cleanrl_utils.evals.td3_eval import evaluate
-
-            episodic_returns = evaluate(
-                model_path,
-                make_env,
-                args.env_id,
-                eval_episodes=10,
-                run_name=f"{run_name}-eval",
-                Model=(Actor, QNetwork),
-                device=device,
-                exploration_noise=args.exploration_noise,
-            )
-            for idx, episodic_return in enumerate(episodic_returns):
-                wrtier.add_scalar("eval/episodic_return", episodic_return, idx)
-
-            if args.upload_model:
-                from cleanrl_utils.huggingface import push_to_hub
-
-                repo_name = f"{args.env_id}-{args.exp_name}-{args.seed}"
-                repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
-                push_to_hub(
-                    args,
-                    episodic_returns,
-                    repo_id,
-                    "TD3",
-                    f"runs/{run_name}",
-                    f"videos/{run_name}-eval"
-                )
-
+            if global_step % 20 == 0:
+                print(f"# of step:{global_step}, avg score : {score/20:1f}")
+                score = 0.0
     envs.close()
-    writer.close()
